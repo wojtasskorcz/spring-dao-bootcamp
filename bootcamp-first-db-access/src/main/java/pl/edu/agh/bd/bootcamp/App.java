4aa3734040817bba2c5de29b2bb0f06253c54c2a
bryk <bryk@student.agh.edu.pl>
2013-04-03 01:48:22 +0200

    Implemented first db access tutorial step. It consists of Customer class and table.

--- bootcamp-first-db-access/src/main/java/pl/edu/agh/bd/bootcamp/App.java
+++ bootcamp-first-db-access/src/main/java/pl/edu/agh/bd/bootcamp/App.java
@@ -5,26 +5,29 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import pl.edu.agh.bd.bootcamp.dummy.dao.DummyDao;
-import pl.edu.agh.bd.bootcamp.dummy.model.Dummy;
+import pl.edu.agh.bd.bootcamp.dao.CustomerDao;
+import pl.edu.agh.bd.bootcamp.model.Customer;
 
 public class App {
-	
+
 	public static void main(String[] args) {
-		ApplicationContext appContext = 
-		    	  new ClassPathXmlApplicationContext("config/BeanLocations.xml");
-		
-		DummyDao dummyDao = (DummyDao)appContext.getBean("dummyDao");
-		
-		Dummy d1 = new Dummy("Wojciech", 21);
-		Dummy d2 = new Dummy("Piotr", 21);
-		dummyDao.save(d1);
-		dummyDao.save(d2);
-		
-		List<Dummy> dummies = dummyDao.getAll();
-		
-		for (Dummy d : dummies) {
-			System.out.println(d);
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(
+				"config/BeanLocations.xml");
+
+		CustomerDao customerDao = (CustomerDao) appContext
+				.getBean("customerDao");
+
+		Customer c1 = new Customer();
+		c1.setContactName("Piotr");
+		Customer c2 = new Customer();
+		c2.setContactName("Wojciech");
+		customerDao.save(c1);
+		customerDao.save(c2);
+
+		List<Customer> customers = customerDao.getAll();
+
+		for (Customer c : customers) {
+			System.out.println(c);
 		}
-	}	
+	}
 }
